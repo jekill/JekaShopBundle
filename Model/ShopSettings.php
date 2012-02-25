100326a9a5806a6886f34d6fa6cc66cb7bd0a61c
Eugeny Fomin <github.com@jeka.ru>
2012-02-26 01:42:51 +0400

    added analytics code to layout

--- Model/ShopSettings.php
+++ Model/ShopSettings.php
@@ -4,15 +4,17 @@ namespace Jeka\ShopBundle\Model;
 class ShopSettings
 {
 
+    private $container;
     private $siteName = 'Альбомчик.Ру';
     private $contacts = array(
         'phones' => '+7 (499) 3434 1 34'
     );
 
 
-    public function __construct()
+    public function __construct($container)
     {
         //$this->siteName=$sitename
+        $this->container = $container;
     }
 
     public function getContacts()
@@ -27,5 +29,9 @@ class ShopSettings
 
     public function getSiteLogoDescription(){
         return 'Магазин фототоваров';
+    }
+
+    public function getAnalyticsCode(){
+        return $this->container->getParameter('jeka_shop.analytics');
     }
 }
--- Twig/ShopSettingsExtension.php
+++ Twig/ShopSettingsExtension.php
@@ -3,13 +3,20 @@
 namespace Jeka\ShopBundle\Twig;
 
 use \Jeka\ShopBundle\Model\ShopSettings;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
-class ShopSettingsExtension extends  \Twig_Extension{
+class ShopSettingsExtension extends  \Twig_Extension {
+
+    private $settings;
+
+    function __construct(ShopSettings $settings){
+        $this->settings=$settings;
+    }
 
     public function getGlobals()
     {
         return array(
-            'settings'=> new ShopSettings()
+            'settings'=> $this->settings
         );
     }
 
